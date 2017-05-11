a9cea41effcc43f221e67a548ca2e4006da13f6a
tobiskillz <alagbet@yahoomail.com>
2017-05-11 15:30:44 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,200 @@
+
+// setInterval(function(){
+//   $(".prev").trigger("click");
+// },6000);
+
+// $(".a").trigger("click");
+
+
+
+
+
+
+// $(".hero-main-text").fadeIn(4000);
+
+// $(".hero-sub-text").fadeIn(5800);
+
+// $(".hero-button").fadeIn(6000);
+
+
+
+
+// window.onscroll = function(){
+
+// 		if ( $('.welcome').visible(true) ) {
+//           $(".welcomebody").fadeIn(5000);
+//           $(".matcont").fadeIn(5000);
+//           $(".sig").fadeIn(15000);
+// 		}
+
+// };
+
+
+
+
+// function hoverEffect (elem){
+
+// 	  $(elem).hover(function(){
+// 	  var overlay =	$(elem).children();
+// 	  overlay.animate({opacity: '0.8'},500);
+// 	}).mouseleave(function(){
+// 	  var overlay =	$(elem).children();
+// 	  overlay.animate({opacity: '0.3'},50);
+// 	})
+
+// };
+
+
+// hoverEffect(".box1");
+// hoverEffect(".box2");
+// hoverEffect(".box3");
+// hoverEffect(".box4");
+
+
+// var clickthru = $(".next");
+// var counter = 1;
+
+// clickthru.click(function(event){
+//    event.preventDefault();
+   
+//    if (counter != 12 ) {
+// 	   var newimg = counter++;
+
+// 	   $("#theimg").attr("src","img/"+newimg+".jpg");
+//    }else{
+//    	 counter = 1;
+//    }
+
+
+
+// });
+
+
+
+
+// var clickthru = $(".next2");
+// var counter = 1;
+
+// clickthru.click(function(event){
+//    event.preventDefault();
+   
+//    if (counter != 12 ) {
+// 	   var newimg = counter++;
+
+// 	   $("#theimg2").attr("src","img/"+newimg+".jpg");
+//    }else{
+//    	 counter = 1;
+//    }
+
+
+
+// });
+
+
+
+
+//  function scrollTo(elem) {
+//     $('html, body').animate({ scrollTop: $(elem).offset().top }, 'slow');
+//     return false;
+//  };
+
+
+//  $("#about").click(function(){
+//  	scrollTo(".welcomebox")
+//  })
+
+
+//   $("#about2").click(function(e){
+//   	e.preventDefault();
+//  	scrollTo(".welcomebox")
+//  })
+
+
+//   $("#contact").click(function(){
+//  	scrollTo(".contactrow")
+//  })
+
+
+// // var windowWidth = $(window).width();
+
+// //  $(window).resize(function(){
+
+// //    // console.log($(window).width());
+// //    if (windowWidth < 755) {
+// //      $(".mainmenu").css("display", "none");
+// //      $(".welcomebody-cont").css("padding-left","20px");
+// //      $(".welcomebody-cont").css("padding-right","20px");
+// //      $(".welcomebody").css("text-align","center");
+// //    }
+
+
+
+// // });
+
+
+// //  $(window).resize(function(){
+
+// //    // console.log($(window).width());
+// //    if (windowWidth > 755) {
+// //      $(".welcomebody").css("text-align","justify");
+// //    }
+
+
+
+// // });
+
+
+
+
+
+// // var marker = 1;
+// // $("#clickmenu").click(function(){
+// //    if (marker == 1) {
+// //    	 $(".mainmenu").css("display", "none");
+// //    	 marker = 0;
+// //    }else{
+// //    	 $(".mainmenu").css("display", "block");
+// //    	 marker = 1; 
+// //    }
+
+// // })
+
+
+
+
+// // console.log($(window).width());
+
+
+// if ( $(window).width() < 700 ) {
+    
+//     var marker = 0;
+// 	$("#clickmenu").click(function(){
+		
+// 		if (marker == 0) {
+// 		    $(".mainmenu").css("display", "block");
+// 		    marker = 1;
+// 		}else{
+// 			$(".mainmenu").css("display", "none");
+// 		    marker = 0;	
+// 		}
+
+// 	}) //end of click function
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
